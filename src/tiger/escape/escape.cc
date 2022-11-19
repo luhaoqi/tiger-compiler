f4976e3ea09e34bf7b52525c84c131c5dc1326fa
luhaoqi <1171821456@qq.com>
2022-11-20 01:31:37 +0800

    修改FindEscape，增加注释

--- src/tiger/escape/escape.cc
+++ src/tiger/escape/escape.cc
@@ -3,10 +3,7 @@
 #include "tiger/absyn/absyn.h"
 
 namespace esc {
-void EscFinder::FindEscape() {
-  sym::Table<EscapeEntry>* env = new sym::Table<EscapeEntry>();
-  absyn_tree_->Traverse(env);
-}
+void EscFinder::FindEscape() { absyn_tree_->Traverse(env_.get()); }
 }  // namespace esc
 
 namespace absyn {
@@ -19,6 +16,7 @@ void AbsynTree::Traverse(esc::EscEnvPtr env) {
 
 void SimpleVar::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  // 看到变量，查看是否在更深层，是的话说明是逃逸变量
   esc::EscapeEntry* t = env->Look(sym_);
   if (depth > t->depth_) *(t->escape_) = true;
   return;
@@ -26,12 +24,14 @@ void SimpleVar::Traverse(esc::EscEnvPtr env, int depth) {
 
 void FieldVar::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  // 类似 a.x 遍历a 只关注这个var本身，x只是个symbol
   var_->Traverse(env, depth);
   return;
 }
 
 void SubscriptVar::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  // a[x] 同时检查 a 和 x
   var_->Traverse(env, depth);
   subscript_->Traverse(env, depth);
   return;
@@ -60,6 +60,7 @@ void StringExp::Traverse(esc::EscEnvPtr env, int depth) {
 
 void CallExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  // e.g. f(x,y,z,w) check x,y,z,w
   auto list = args_->GetList();
   for (auto ele : list) ele->Traverse(env, depth);
   return;
@@ -74,6 +75,7 @@ void OpExp::Traverse(esc::EscEnvPtr env, int depth) {
 
 void RecordExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  // node{val = x, next = nxt}  check x & nxt
   auto list = fields_->GetList();
   for (auto ele : list) ele->exp_->Traverse(env, depth);
   return;
@@ -81,6 +83,7 @@ void RecordExp::Traverse(esc::EscEnvPtr env, int depth) {
 
 void SeqExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  // check each exp
   auto list = seq_->GetList();
   for (auto ele : list) ele->Traverse(env, depth);
   return;
@@ -88,6 +91,7 @@ void SeqExp::Traverse(esc::EscEnvPtr env, int depth) {
 
 void AssignExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  // a:=b check a&b
   var_->Traverse(env, depth);
   exp_->Traverse(env, depth);
   return;
@@ -95,6 +99,8 @@ void AssignExp::Traverse(esc::EscEnvPtr env, int depth) {
 
 void IfExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  // if (x > 2) then a:=2 else b:=c
+  // check both exp
   test_->Traverse(env, depth);
   then_->Traverse(env, depth);
   if (elsee_) elsee_->Traverse(env, depth);
@@ -103,6 +109,7 @@ void IfExp::Traverse(esc::EscEnvPtr env, int depth) {
 
 void WhileExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  // while (x > 2) do a:=a+1
   test_->Traverse(env, depth);
   body_->Traverse(env, depth);
   return;
@@ -110,6 +117,9 @@ void WhileExp::Traverse(esc::EscEnvPtr env, int depth) {
 
 void ForExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  // for 需要增加一个变量声明
+  // for i:=1 to n do a:=a+1
+  // 这个escape是ForExp自己的一个成员变量
   escape_ = false;
   env->Enter(var_, new esc::EscapeEntry(depth, &(escape_)));
   lo_->Traverse(env, depth);
@@ -145,11 +155,15 @@ void VoidExp::Traverse(esc::EscEnvPtr env, int depth) {
 
 void FunctionDec::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  // FunDec 唯一可能dep++
   auto fuclist = functions_->GetList();
   for (auto fuc : fuclist) {
+    // 同层的函数是互相看不到局部变量的 所以用BeginScope和EndScope区分可见度
     env->BeginScope();
     auto paramlist = fuc->params_->GetList();
+    // function merge(a:list, b:list):list
     for (auto param : paramlist) {
+      // a:list 代表的是一个Field，也自带escape属性，专门表示函数形参的逃逸属性
       param->escape_ = false;
       env->Enter(param->name_,
                  new esc::EscapeEntry(depth + 1, &param->escape_));
@@ -162,6 +176,9 @@ void FunctionDec::Traverse(esc::EscEnvPtr env, int depth) {
 
 void VarDec::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  // 除了函数声明 dep都不会变
+  // VarDec 的任务就是为env加入新的内容
+  // VarDec 中也有escape成员变量表示这个Var的逃逸属性
   escape_ = false;
   env->Enter(var_, new esc::EscapeEntry(depth, &escape_));
   init_->Traverse(env, depth);
