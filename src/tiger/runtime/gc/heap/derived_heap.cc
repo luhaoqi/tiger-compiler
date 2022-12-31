f5dacbf54a24901cfa6d0c9fcec26d5975160865
luhaoqi <1171821456@qq.com>
2022-12-31 13:55:28 +0800

    lab7: 添加一些框架 + dos2unix

--- src/tiger/runtime/gc/heap/derived_heap.cc
+++ src/tiger/runtime/gc/heap/derived_heap.cc
@@ -1,9 +1,20 @@
 #include "derived_heap.h"
-#include <stdio.h>
-#include <stack>
 #include <cstring>
+#include <stack>
+#include <stdio.h>
 
 namespace gc {
-// TODO(lab7): You need to implement those interfaces inherited from TigerHeap correctly according to your design.
-} // namespace gc
+// TODO(lab7): You need to implement those interfaces inherited from TigerHeap
+// correctly according to your design.
 
+void DerivedHeap::Initialize(uint64_t size) {}
+
+char *DerivedHeap::Allocate(uint64_t size) {}
+
+uint64_t DerivedHeap::Used() const {}
+
+uint64_t DerivedHeap::MaxFree() const {}
+
+void DerivedHeap::GC() {}
+
+} // namespace gc
--- src/tiger/runtime/gc/heap/derived_heap.h
+++ src/tiger/runtime/gc/heap/derived_heap.h
@@ -6,8 +6,55 @@
 namespace gc {
 
 class DerivedHeap : public TigerHeap {
-  // TODO(lab7): You need to implement those interfaces inherited from TigerHeap correctly according to your design.
+  // TODO(lab7): You need to implement those interfaces inherited from TigerHeap
+  // correctly according to your design.
+  /**
+   * Allocate a contiguous space from heap.
+   * If your heap has enough space, just allocate and return.
+   * If fails, return nullptr.
+   * @param size size of the space allocated, in bytes.
+   * @return start address of allocated space, if fail, return nullptr.
+   */
+  virtual char *Allocate(uint64_t size);
+
+  /**
+   * Acquire the total allocated space from heap.
+   * Hint: If you implement a contigous heap, you could simply calculate the
+   * distance between top and bottom, but if you implement in a link-list way,
+   * you may need to sum the whole free list and calculate the total free space.
+   * We will evaluate your work through this, make sure you implement it
+   * correctly.
+   * @return total size of the allocated space, in bytes.
+   */
+  virtual uint64_t Used() const;
+
+  /**
+   * Acquire the longest contiguous space from heap.
+   * It should at least be 50% of inital heap size when initalization.
+   * However, this may be tricky when you try to implement a Generatioal-GC, it
+   * depends on how you set the ratio between Young Gen and Old Gen, we suggest
+   * you making sure Old Gen is larger than Young Gen. Hint: If you implement a
+   * contigous heap, you could simply calculate the distance between top and
+   * end, if you implement in a link-list way, you may need to find the biggest
+   * free list and return it. We use this to evaluate your utilzation, make sure
+   * you implement it correctly.
+   * @return size of the longest , in bytes.
+   */
+  virtual uint64_t MaxFree() const;
+
+  /**
+   * Initialize your heap.
+   * @param size size of your heap in bytes.
+   */
+  virtual void Initialize(uint64_t size);
+
+  /**
+   * Do Garbage collection!
+   * Hint: Though we do not suggest you implementing a Generational-GC due to
+   * limited time, if you are willing to try it, add a function named YoungGC,
+   * this will be treated as FullGC by default.
+   */
+  virtual void GC();
 };
 
 } // namespace gc
-
